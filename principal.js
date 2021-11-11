51765b8b0986a92f7e6fc5716c9127ea76cb8c18
Edgar Aragão <70443366+edgararagao@users.noreply.github.com>
2021-11-11 11:06:56 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,16 @@
+var nome = "Edgar"
+
+var notaDoPrimeiroBimestre = 7
+var notaDoSegundoBimestre = 9
+var notaDoTerceiroBimestre = 6
+var notaDoQuartoBimestre = 10
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4 
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log("Sua média é: " + notaFixada)
+
+//Revisão
+//Variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
